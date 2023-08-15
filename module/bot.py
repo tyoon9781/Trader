88fc439eadb730b14efa3842f944bba8969eef34
ty <tyoon9781@gmail.com>
2023-08-16 00:56:04 +0900

    [add] qdarkstyle, auto trade basic frame

--- module/bot.py
+++ module/bot.py
@@ -4,7 +4,6 @@ import time
 from datetime import datetime
 
 
-
 class MyBot:
     def __init__(self):
         kiwoom = Kiwoom()
@@ -13,12 +12,13 @@ class MyBot:
         self.kw = kiwoom
         self.account_count = self.kw.GetLoginInfo("ACCOUNT_CNT")
         self.account_list = self.kw.GetLoginInfo("ACCNO")
+        self.account, = self.account_list
         self.user_id = self.kw.GetLoginInfo("USER_ID")
         self.user_name = self.kw.GetLoginInfo("USER_NAME")
         self.keyboard_secure_exception = bool(self.kw.GetLoginInfo("KEY_BSECGB"))
         self.waterwall_setup = bool(self.kw.GetLoginInfo("FIREW_SECGB"))
         self.connection = bool(self.kw.GetConnectState())
-        self.account, = self.account_list
+        self.conditions:pd.DataFrame = self.get_conditions()
         self.init()
 
     def init(self):
@@ -30,178 +30,19 @@ class MyBot:
         print("방화벽 설정 여부 :", bool(self.kw.GetLoginInfo("FIREW_SECGB")))
         print("연결 여부 :", bool(self.kw.GetConnectState()))
 
-    def get_theme_table(self, format: str="gc"):
-        if format == "gc":
-            print(self.kw.GetThemeGroupList(1))  ## {그룹명 : 코드}
-        elif format == "cg":
-            print(self.kw.GetThemeGroupList(0))  ## {코드 : 그룹명}
-        else:
-            print("no match format")
-
-    def get_condition_info(self):
-        # 조건 얻기
+    def get_conditions(self):
         self.kw.GetConditionLoad()
-        # 전체 조건식 리스트 얻기
-        conditions = self.kw.GetConditionNameList()
-        # 0번 조건식에 해당하는 종목 리스트 출력
-        condition_index = conditions[0][0]
-        condition_name = conditions[0][1]
-        codes = self.kw.SendCondition("0101", condition_name, condition_index, 0)
-        print(codes)
+        return pd.DataFrame(self.kw.GetConditionNameList(), columns=["index", "name"])
 
-    def get_etf_market_info(self) -> pd.DataFrame:
-        etf = self.kw.GetCodeListByMarket('8')
-        return self._get_detail_info_from_codes(etf)
-
-    def get_kosdac_market_info(self) -> pd.DataFrame:
-        kosdaq = self.kw.GetCodeListByMarket('10')
-        return self._get_detail_info_from_codes(kosdaq)
-
-    def get_kospi_market_info(self) -> pd.DataFrame:
-        kospi = self.kw.GetCodeListByMarket('0')
-        return self._get_detail_info_from_codes(kospi)
-
-    def _get_detail_info_from_codes(self, codes: list) -> pd.DataFrame:
-        result = []
-        for code in codes:
-            c0 = code
-            c1 = self.kw.GetMasterCodeName(code)         ## 종목명
-            c2 = self.kw.GetMasterListedStockCnt(code)   ## 주식 수
-            c3 = self.kw.GetMasterConstruction(code)     ## 감리구분
-            c4 = self.kw.GetMasterListedStockDate(code)  ## 상장일
-            c5 = self.kw.GetMasterLastPrice(code)        ## 전일가
-            c6 = self.kw.GetMasterStockState(code)       ## 종목상태
-            result.append([c0, c1, c2, c3, c4, c5, c6])
-        df = pd.DataFrame(result, columns=["코드", "종목명", "주식 수", "감리구분", "상장일", "전일가", "종목상태"])
-        return df
-
-    def order_buy_market_price(self, code, quantity):
-        """
-        회사, 주식량 시장가 매수
-        """
-        print(datetime.now())
-        try:
-            if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-                result = self.kw.SendOrder(rqname="시장가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=0, hoga="03", order_no="")
-                print(result)
-                print(f"[Info] [order_buy_market_price] {code}, {quantity}주, 시장가 buy order has been placed")
-            else:
-                print(f"[Error] [order_buy_market_price] {code} code or {quantity} quantity is incorrect")
-        except Exception as e:
-            print(f"[Error] {e}")
-
-    def order_sell_market_price(self, code, quantity):
-        """
-        회사, 주식량 시장가 매도
-        """
-        print(datetime.now())
-        try:
-            if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-                result = self.kw.SendOrder(rqname="시장가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=0, hoga="03", order_no="")
-                print(result)
-                print(f"[Info] [order_sell_market_price] {code}, {quantity}주, 시장가 sell order has been placed")
-            else:
-                print(f"[Error] [order_sell_market_price] {code} code or {quantity} quantity is incorrect")
-        except Exception as e:
-            print(f"[Error] {e}")
-
-
-    def order_buy_custom_price(self, code, quantity, price):
-        """
-        회사, 주식량, 지정가 매수
-        """
-        print(datetime.now())
-        try:
-            if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-                result = self.kw.SendOrder(rqname="지정가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=price, hoga="00", order_no="")
-                print(result)
-                print(f"[Info] [order_buy_custom_price] {code}, {quantity}주, {price}원 buy order has been placed")
-            else:
-                print(f"[Error] [order_buy_custom_price] {code} code or {quantity} quantity is incorrect")
-        except Exception as e:
-            print(f"[Error] {e}")
-
-    def order_sell_custom_price(self, code, quantity, price):
-        """
-        회사, 주식량, 지정가 매도
-        """
-        print(datetime.now())
-        try:
-            if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-                result = self.kw.SendOrder(rqname="지정가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=price, hoga="00", order_no="")
-                print(result)
-                print(f"[Info] [order_sell_custom_price] {code}, {quantity}주, {price}원 sell order has been placed")
-            else:
-                print(f"[Error] [order_sell_custom_price] {code} code or {quantity} quantity is incorrect")
-        except Exception as e:
-            print(f"[Error] {e}")
-
-
-    def cancel_buy_market_price(self, code, quantity, order_no):
-        """
-        회사, 주식량 시장가 매수 취소
-        """
-        print(datetime.now())
-        try:
-            if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-                if self.is_exist_order_no(order_no):
-                    result = self.kw.SendOrder(rqname="시장가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=0, hoga="03", order_no=order_no)
-                    print(result)
-                    print(f"[Info] [cancel_buy_market_price] {order_no}, {code}, {quantity}주 buy order cancel complete")
-                else:
-                    print(f"[Warning] [cancel_buy_market_price] {order_no} order no is not exist")
-            else:
-                print(f"[Error] [cancel_buy_market_price] {code} code or {quantity} quantity is incorrect")
-        except Exception as e:
-            print(f"[Error] {e}")
-
-    def cancel_sell_market_price(self, code, quantity, order_no):
-        """
-        회사, 주식량 시장가 매도 취소
-        """
-        print(datetime.now())
-        if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-            if self.is_exist_order_no(order_no):
-                result = self.kw.SendOrder(rqname="시장가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=0, hoga="03", order_no=order_no)
-                print(result)
-                print(f"[Info] [cancel_sell_market_price] {order_no}, {code}, {quantity}주, 시장가 sell order cancel complete")
-            else:
-                print(f"[Warning] [cancel_sell_market_price] {order_no} order no is not exist") 
-        else:
-            print(f"[Error] [cancel_sell_market_price] {code} code or {quantity} quantity is incorrect")
-
-
-    def cancel_buy_custom_price(self, code, quantity, price, order_no):
-        """
-        회사, 주식량, 지정가 매수 취소
-        """
-        print(datetime.now())
-        if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-            if self.is_exist_order_no(order_no):
-                result = self.kw.SendOrder(rqname="지정가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=price, hoga="00", order_no=order_no)
-                print(result)
-                print(f"[Info] [cancel_buy_custom_price] {order_no}, {code}, {quantity}주, {price}원 buy order cancel complete")
-            else:
-                print(f"[Warning] [cancel_buy_custom_price] {order_no} order no is not exist") 
-        else:
-            print(f"[Error] [cancel_buy_custom_price] {code} code or {quantity} quantity is incorrect")
-
-
-    def cancel_sell_custom_price(self, code, quantity, price, order_no):
-        """
-        회사, 주식량, 지정가 매도 취소
-        """
-        print(datetime.now())
-        if self.is_code_valid(code) and self.is_order_number_valid(quantity):
-            if self.is_exist_order_no(order_no):
-                result = self.kw.SendOrder(rqname="지정가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=price, hoga="00", order_no=order_no)
-                print(result)
-                print(f"[Info] [cancel_buy_custom_price] {order_no}, {code}, {quantity}주, {price}원 sell order cancel complete")
-            else:
-                print(f"[Warning] [cancel_sell_custom_price] {order_no} order no is not exist") 
-        else:
-            print(f"[Error] [cancel_sell_custom_price] {code} code or {quantity} quantity is incorrect")
-
+    def get_history_day_info_once(self, code) -> pd.DataFrame:
+        return self.kw.block_request("opt10081", 종목코드=code, 기준일자=datetime.now().strftime("%Y%m%d"), 수정주가구분=1, output="주식일봉차트조회", next=0)
+    
+    def get_history_minute_info_once(self, code) -> pd.DataFrame:
+        return self.kw.block_request("opt10080", 종목코드=code, 기준일자=datetime.now().strftime("%Y%m%d"), 수정주가구분=1, output="주식분봉차트조회", next=0)
+    
+    def get_history_tick_info_once(self, code) -> pd.DataFrame:
+        return self.kw.block_request("opt10079", 종목코드=code, 기준일자=datetime.now().strftime("%Y%m%d"), 수정주가구분=1, output="주식틱봉차트조회", next=0)
+    
     def request_day_history_info(self, code, date, count=5):
         """
         code = string
@@ -260,6 +101,87 @@ class MyBot:
     ## todo
     def is_exist_order_no(order_no):
         pass
+
+    ######################################
+    # well-made methods
+    ######################################
+    def get_basic_info_themes(self, format: str="gc"):
+        if format == "gc":
+            print(self.kw.GetThemeGroupList(1))  ## {그룹명 : 코드}
+        elif format == "cg":
+            print(self.kw.GetThemeGroupList(0))  ## {코드 : 그룹명}
+        else:
+            print("no match format")
+
+    def get_account_info_conditional_expression(self, name):
+        index = self.conditions.loc[self.conditions["name"] == name].iloc[0]["index"]
+        return self.kw.SendCondition("0101", name, index, 0)
+
+    def get_market_info_etf(self) -> pd.DataFrame:
+        etf = self.kw.GetCodeListByMarket('8')
+        return self._get_detail_info_from_codes(etf)
+
+    def get_market_info_kosdac(self) -> pd.DataFrame:
+        kosdaq = self.kw.GetCodeListByMarket('10')
+        return self._get_detail_info_from_codes(kosdaq)
+
+    def get_market_info_kospi(self) -> pd.DataFrame:
+        _info = self.kw.GetCodeListByMarket('0')
+        mutual = self.kw.GetCodeListByMarket('4')
+        reits = self.kw.GetCodeListByMarket('6')
+        etf = self.kw.GetCodeListByMarket('8')
+        delete_list = mutual + reits + etf
+        kospi = [x for x in _info if x not in delete_list]
+        return self._get_detail_info_from_codes(kospi)
+
+    def get_market_info_kosdac_n_kospi(self) -> pd.DataFrame:
+        return pd.concat([self.get_market_info_kospi(), self.get_market_info_kosdac()])
+
+    def _get_detail_info_from_codes(self, codes: list) -> pd.DataFrame:
+        result = []
+        for code in codes:
+            c0 = code
+            c1 = self.kw.GetMasterCodeName(code)         ## 종목명
+            c2 = self.kw.GetMasterListedStockCnt(code)   ## 주식 수
+            c3 = self.kw.GetMasterConstruction(code)     ## 감리구분
+            c4 = self.kw.GetMasterListedStockDate(code)  ## 상장일
+            c5 = self.kw.GetMasterLastPrice(code)        ## 전일가
+            c6 = self.kw.GetMasterStockState(code)       ## 종목상태
+            result.append([c0, c1, c2, c3, c4, c5, c6])
+        df = pd.DataFrame(result, columns=["code", "종목명", "주식 수", "감리구분", "상장일", "전일가", "종목상태"])
+        return df
+
+    def order_buy_market_price(self, code, quantity):
+        """ 회사, 주식량 시장가 매수 """
+        self.kw.SendOrder(rqname="시장가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=0, hoga="03", order_no="")
+
+    def order_sell_market_price(self, code, quantity):
+        """ 회사, 주식량 시장가 매도 """
+        self.kw.SendOrder(rqname="시장가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=0, hoga="03", order_no="")
+
+    def order_buy_custom_price(self, code, quantity, price):
+        """ 회사, 주식량, 지정가 매수 """
+        self.kw.SendOrder(rqname="지정가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=price, hoga="00", order_no="")
+
+    def order_sell_custom_price(self, code, quantity, price):
+        """ 회사, 주식량, 지정가 매도 """
+        self.kw.SendOrder(rqname="지정가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=price, hoga="00", order_no="")
+
+    def cancel_buy_market_price(self, code, quantity, order_no):
+        """ 회사, 주식량 시장가 매수 취소 """
+        self.kw.SendOrder(rqname="시장가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=0, hoga="03", order_no=order_no)
+
+    def cancel_sell_market_price(self, code, quantity, order_no):
+        """ 회사, 주식량 시장가 매도 취소 """
+        self.kw.SendOrder(rqname="시장가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=0, hoga="03", order_no=order_no)
+
+    def cancel_buy_custom_price(self, code, quantity, price, order_no):
+        """ 회사, 주식량, 지정가 매수 취소 """
+        self.kw.SendOrder(rqname="지정가매수", screen="0101", accno=self.account, order_type=1, code=code, quantity=quantity, price=price, hoga="00", order_no=order_no)
+
+    def cancel_sell_custom_price(self, code, quantity, price, order_no):
+        """ 회사, 주식량, 지정가 매도 취소 """
+        self.kw.SendOrder(rqname="지정가매도", screen="0101", accno=self.account, order_type=2, code=code, quantity=quantity, price=price, hoga="00", order_no=order_no)
 
     @staticmethod
     def is_code_valid(code: str):
--- trader/views.py
+++ trader/views.py
@@ -1,38 +1,64 @@
-from PyQt5.QtWidgets import QMainWindow
-import pandas as pd
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 
 from trader.form import Ui_MainWindow
 from module.bot import MyBot
 
 
-class Trader(QMainWindow):
+class Trader(QtWidgets.QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         self.my_bot = MyBot()
 
         self.market_price_flag = False
-        self.kosdac_info = self.my_bot.get_kosdac_market_info()
-        self.kospi_info = self.my_bot.get_kospi_market_info()
-        self.etf_info = self.my_bot.get_etf_market_info()
-        self.market_info = pd.concat([self.kosdac_info, self.kospi_info])
+        self.current_buy_volume = 0
+        self.current_sell_volume = 0
+        self.current_proceeds = 0
+        self.current_auto_trade_start_time = self.ui.dateTimeEdit_auto_trade_start_time.dateTime().time()
+        self.current_auto_trade_end_time = self.ui.dateTimeEdit_auto_trade_end_time.dateTime().time()
+        self.current_max_buy_stock_num = int(self.ui.lineEdit_max_buy_stock_num.text())
+        self.current_total_amount_per_stock = int(self.ui.lineEdit_total_amount_per_stock.text().replace(",", ""))
+        self.current_total_sell_adv_ratio = float(self.ui.lineEdit_total_sell_adv_ratio.text())
+        self.current_total_sell_dis_ratio = float(self.ui.lineEdit_total_sell_dis_ratio.text())
+        self.current_total_sell_time = self.ui.dateTimeEdit_total_sell_time.dateTime().time()
+        self.market_info = self.my_bot.get_market_info_kosdac_n_kospi()
         self.current_company_code = "000000"
+        self.condition_table_column_checkbox = QtWidgets.QCheckBox()
+        self.condition_table_checkbox_list = []
+        self.condition_table_all_checked = False
+        self.condition_table_sorted_reverse = False
 
         self.init()
     
     def init(self):
-        ## connect
+        ## connect auto page
+        self.ui.pushButton_auto_trading_start.clicked.connect(self.click_pushButton_auto_trading_start)
+        self.ui.pushButton_auto_trading_end.clicked.connect(self.click_pushButton_auto_trading_end)
+        self.ui.pushButton_select_conditional.clicked.connect(self.click_pushButton_select_conditional)
+        ## get basic info
+        self._update_label_buy_volume()
+        self._update_label_sell_volume()
+        self._update_label_proceeds()
+        self._update_comboBox_conditional()
+        
+        ## change conditional_table_column_checkbox
+        self.ui.tableWidget_contitional_search.horizontalHeader().sectionClicked.connect(self.click_tableWidget_contitional_search)
+
+        ##########################
+        ## connect manual page
+        ##########################
         self.ui.pushButton_buy_order.clicked.connect(self.click_pushButton_buy_order)
         self.ui.pushButton_sell_order.clicked.connect(self.click_pushButton_sell_order)
         self.ui.pushButton_fix_order.clicked.connect(self.click_pushButton_sell_order)
         self.ui.pushButton_cancel_order.clicked.connect(self.click_pushButton_cancle_order)
-        self.ui.checkBox_market_price.stateChanged.connect(self.change_checkbox_market_price)
         self.ui.pushButton_search_company.clicked.connect(self.click_pushButton_search_company)
+        self.ui.checkBox_market_price.stateChanged.connect(self.change_checkbox_market_price)
 
         ## get basic info
-        self._update_label_company_code()        
+        self._update_label_company_code()
         self.ui.label_user_id.setText(f"아이디 : {self.my_bot.user_id}")
         self.ui.label_user_name.setText(f"이름 : {self.my_bot.user_name}")
         self.ui.label_account_count.setText(f"계좌수 : {self.my_bot.account_count}")
@@ -43,8 +69,69 @@ class Trader(QMainWindow):
         self.ui.label_connection.setText(f"연결 : {self.my_bot.connection}")
         self.ui.lineEdit_company_name.setPlaceholderText("종목명")
 
+    def click_tableWidget_contitional_search(self, column):
+        if column == 0:
+            self.condition_table_all_checked = not self.condition_table_all_checked
+            for checkbox in self.condition_table_checkbox_list:
+                checkbox: QtWidgets.QCheckBox
+                checkbox.setChecked(self.condition_table_all_checked)
+        else:
+            self.condition_table_sorted_reverse = not self.condition_table_sorted_reverse
+            if self.condition_table_sorted_reverse:
+                self.ui.tableWidget_contitional_search.sortItems(column, order=Qt.SortOrder.AscendingOrder)
+            else:
+                self.ui.tableWidget_contitional_search.sortItems(column, order=Qt.SortOrder.DescendingOrder)
+
+    def click_pushButton_select_conditional(self):
+        current_conditional_text = self.ui.comboBox_conditional.currentText()
+        code_list = self.my_bot.get_account_info_conditional_expression(current_conditional_text)
+        self.ui.tableWidget_contitional_search.setRowCount(len(code_list))
+        self.condition_table_checkbox_list = []
+        for i in range(len(code_list)):
+            self.condition_table_checkbox_list.append(QtWidgets.QCheckBox())
+        for row_idx, code in enumerate(code_list):
+            row_data = [
+                "checkbox",
+                "상태",
+                code,
+                self.my_bot.kw.GetMasterCodeName(code),     ## 종목 명
+                "구문",
+                self.my_bot.kw.GetCommRealData(code, 11),   ## 전일 대비
+                self.my_bot.kw.GetCommRealData(code, 10),   ## 현재가
+                self.my_bot.kw.GetCommRealData(code, 12),   ## 등락율
+                self.my_bot.kw.GetCommRealData(code, 13),   ## 누적거래량
+                "편입가",
+                "편입대비",
+                "수익률"
+            ]
+            for col_idx, item_data in enumerate(row_data):
+                if col_idx == 0:
+                    self.ui.tableWidget_contitional_search.setCellWidget(row_idx, col_idx, self.condition_table_checkbox_list[row_idx])
+                else:
+                    item = QtWidgets.QTableWidgetItem(str(item_data))
+                    self.ui.tableWidget_contitional_search.setItem(row_idx, col_idx, item)
+        print("click_pushButton_select_conditional")
+
+    def click_pushButton_auto_trading_end(self):
+        print("click_pushButton_auto_trading_end")
+
+    def click_pushButton_auto_trading_start(self):
+        print("click_pushButton_auto_trading_start")
+
+    def _update_comboBox_conditional(self):
+        self.ui.comboBox_conditional.addItems(self.my_bot.conditions["name"])
+
+    def _update_label_buy_volume(self):
+        self.ui.label_buy_volume.setText(str(self.current_buy_volume))
+
+    def _update_label_sell_volume(self):
+        self.ui.label_sell_volume.setText(str(self.current_sell_volume))
+
+    def _update_label_proceeds(self):
+        self.ui.label_proceeds.setText(str(self.current_proceeds))
+
     def _update_label_company_code(self):
-        self.ui.label_company_code.setText(self.current_company_code)
+        self.ui.label_company_code.setText(str(self.current_company_code))
 
     def _get_quantity(self):
         quantity = self.ui.lineEdit_order_quantity.text()
